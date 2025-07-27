d1df41101f6c05437e28bcf8f693ef388a4692a4
Shagunk20 <techshah0020@gmail.com>
2025-07-28 01:45:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -342,6 +342,6 @@ io.on('connection', (socket) => {
 });
 
 
-server.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
